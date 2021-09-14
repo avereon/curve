639806386b886469f7117d3276fe8b6098ccb6f2
msoderquist <msoderquist@mastercontrol.com>
2021-09-14 15:46:33 -0600

    Add intersect plane plane

--- source/main/java/com/avereon/curve/math/Intersection3D.java
+++ source/main/java/com/avereon/curve/math/Intersection3D.java
@@ -1,17 +1,22 @@
 package com.avereon.curve.math;
 
-public class Intersection3D  extends Intersection {
+public class Intersection3D extends Intersection {
 
 	protected Intersection3D( Type status, double[]... points ) {
 		super( status, points );
 	}
 
-	public static Intersection3D intersectionLinePlane( double[] a, double[] b, double[] o, double[] n ) {
+	public static Intersection3D intersectLinePlane( double[] a, double[] b, double[] o, double[] n ) {
 		double den = Vector.dot( Vector.subtract( b, a ), n );
 		if( den == 0 ) return new Intersection3D( Type.NONE );
 
 		double z = (Vector.dot( o, n ) - Vector.dot( a, n )) / den;
 		return new Intersection3D( Type.INTERSECTION, Vector.add( a, Vector.scale( Vector.subtract( b, a ), z ) ) );
+	}
+
+	public static Intersection3D intersectPlanePlane( double[] ao, double[] an, double[] bo, double[] bn ) {
+		// TODO Implement plane-plane intersection
+		return new Intersection3D( Type.NONE );
 	}
 
 }
--- source/test/java/com/avereon/curve/math/Intersection3DTest.java
+++ source/test/java/com/avereon/curve/math/Intersection3DTest.java
@@ -8,20 +8,24 @@ import static org.hamcrest.Matchers.is;
 public class Intersection3DTest {
 
 	@Test
-	public void testGetIntersectionWithPlaneAndLine() {
+	void testIntersectLinePlane() {
 		// Null checks.
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ -1, 0, 0 }, new double[]{ 1, 0, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ -1, 2, 0 }, new double[]{ 1, 2, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ -1, 2, 3 }, new double[]{ 1, 2, 3 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ -1, 0, 0 }, new double[]{ 1, 0, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ -1, 2, 0 }, new double[]{ 1, 2, 0 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ -1, 2, 3 }, new double[]{ 1, 2, 3 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.NONE ) ) );
 
 		// Trivial plane with non-trivial line.
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ 0, 0, -1 }, new double[]{ 0, 0, 1 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 0, 0, 0 } ) ) );
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ -1, -1, -1 }, new double[]{ 1, 1, 1 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 0, 0, 0 } ) ) );
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ 0, 0, 2 }, new double[]{ 0, 0, 1 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 0, 0, 0 } ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ 0, 0, -1 }, new double[]{ 0, 0, 1 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 0, 0, 0 } ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ -1, -1, -1 }, new double[]{ 1, 1, 1 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 0, 0, 0 } ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ 0, 0, 2 }, new double[]{ 0, 0, 1 }, new double[]{ 0, 0, 0 }, new double[]{ 0, 0, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 0, 0, 0 } ) ) );
 
 		// Non-trivial plane with trivial line.
-		assertThat( Intersection3D.intersectionLinePlane( new double[]{ 1, 2, -1 }, new double[]{ 1, 2, 1 }, new double[]{ 1, 2, 3 }, new double[]{ 1, 1, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 1, 2, 3 } ) ) );
+		assertThat( Intersection3D.intersectLinePlane( new double[]{ 1, 2, -1 }, new double[]{ 1, 2, 1 }, new double[]{ 1, 2, 3 }, new double[]{ 1, 1, 1 } ), is( new Intersection3D( Intersection.Type.INTERSECTION, new double[]{ 1, 2, 3 } ) ) );
 	}
 
+	@Test
+	void testIntersectPlanePlane() {
+		// TODO Test Intersection3D.intersectPlanePlane
+	}
 }
